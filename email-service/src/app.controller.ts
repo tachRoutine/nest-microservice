29fdc1c5ece4c2c069c18806763ba0e965adee4d
Tachera Sasi <tacherasasi@gmail.com>
2025-11-06 10:01:58 +0300

    Ellie: add controller

--- email-service/src/app.controller.ts
+++ email-service/src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern({ cmd: 'send_email' })
+  handleSendEmail(@Payload() data: any) {
+    console.log('Received email data:', data);
   }
 }
